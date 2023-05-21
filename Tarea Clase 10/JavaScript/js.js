7ded2e95ad5346ee741ad465713426674927ece9
Djverna <130709243+Djverna@users.noreply.github.com>
2023-05-21 14:30:19 -0300

    Add files via upload

---
+++ Tarea Clase 10/JavaScript/js.js
@@ -0,0 +1,10 @@
+var nombre="David";
+var apellido="Verna";
+console.log(nombre + " " + apellido);
+var numero1= 56;
+var numero2= 24;
+var suma= numero1 + numero2;
+console.log("La suma de numero1 y numero2 es: " + suma);
+var numero3= 38;
+var resultado= suma + numero3;
+console.log("la suma del resultado anterior, más el tercer número es: " + resultado);
